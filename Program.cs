6bc89a619ba056440356c8b771738fe1864e7925
Sajith Dilhan <sajithdilhan@gmail.com>
2022-01-16 23:41:38 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FibonacciExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fib(number));
+            Console.ReadKey();
+        }
+
+        static int Fib(int number)
+        {
+            if (number == 0 || number == 1)
+            {
+                return number;
+            }
+            else 
+            {
+                return (Fib(number - 1) + Fib(number - 2));
+            }
+
+        }
+    }
+}
